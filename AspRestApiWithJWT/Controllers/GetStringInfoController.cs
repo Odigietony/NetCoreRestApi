43c4e5a24a324af4a74ce694edf032a35983b79f
Odigietony <odigietony.jr@gmail.com>
2020-03-11 00:24:07 +0100

    Initial configuration

---
+++ AspRestApiWithJWT/Controllers/GetStringInfoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspRestApiWithJWT.Controllers
+{
+    [Route("api/string_info")]
+    [ApiController]
+    public class GetStringInfoController : ControllerBase
+    {
+        [HttpGet(Name = "StringInfo")]
+        public string StringInfo()
+        {
+            return "Welcome from string info";
+        }
+    }
+}
