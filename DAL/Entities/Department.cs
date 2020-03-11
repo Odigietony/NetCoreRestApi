d16c51d869b4d06e52582795688db79b872d1307
Odigietony <odigietony.jr@gmail.com>
2020-03-11 21:13:35 +0100

    Adding  faculty and department entities

---
+++ DAL/Entities/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DAL.Entities
+{
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public virtual int FacultyId { get; set; }
+        public Faculty Faculty { get; set; }
+    }
+}
