c487072e951b1d13f7833cff52208aad2d8f52aa
Odigietony <odigietony.jr@gmail.com>
2020-03-11 21:15:45 +0100

    Adding a base interface

---
+++ DAL/Interfaces/IBaseInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Interfaces
+{
+    public interface IBaseInterface<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetOne(int Id);
+        void Create(object T);
+        void Update(object T);
+        void Delete(int Id);
+        void Save();
+    }
+}
