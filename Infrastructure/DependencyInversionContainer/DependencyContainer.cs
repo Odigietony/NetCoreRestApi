8ddfd12ba7cd63c47faa689ce631b5f9c10bc3ff
Odigietony <odigietony.jr@gmail.com>
2020-03-16 00:34:19 +0100

    Adding inversion of control

---
+++ Infrastructure/DependencyInversionContainer/DependencyContainer.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Interfaces;
+using Infrastructure.Data;
+using Infrastructure.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.DependencyInversionContainer
+{
+    public class DependencyContainer
+    {
+       public static void RegisterServices(IServiceCollection services)
+       {
+            //Infrastructure Layer
+            services.AddScoped<IFacultyInterface, FacultyRepository>();
+            services.AddScoped<IDepartmentInterface, DepartmentRepository>();
+            services.AddScoped<ApplicationDbContext>();
+       }
+    }
+}
